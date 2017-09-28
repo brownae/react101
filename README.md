546363063efe49ad3cacee3929acacf9d0a19acf
brownae <aebrown9@gmail.com>
2017-09-28 15:40:20 -0700

    Inserts js object into jsx template

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # react101
 A place where I'm going to put all the things about react.js
+
+
+
+Notes:
+From root of folder...
+To reinstall dependencies: $ yarn install
+To run babel and have it watch: $ babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+You can run two terminal windows at the same time.
+To run live server: $ live-server public //public is folder to run.
+
+To template in react create and js object and then inject using {}'s. Then render in the html using ReactDOM.render(name of template, where you want it to go)
+
+Example:
+//////////////////////////
+var user = {
+    name : 'Aaron',
+    Age : 27,
+    Location : 'Seattle, WA'
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.Age}</p>
+        <p>Location: {user.Location}</p>
+    </div>
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+///////////////////////////
--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,12 +2,72 @@
 
 console.log('App.js is running');
 
-//jsx JavaScript XML
+var app = {
+    title: "Indecision App",
+    subtitle: 'Put your life in the hands of a computer'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'dIndecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+
+var user = {
+    name: 'Aaron',
+    Age: 27,
+    Location: 'Seattle, WA'
+};
+
+var username = 'Aaron';
+var userAge = 27;
+var userLocation = 'Seattle, WA';
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.Age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.Location
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
