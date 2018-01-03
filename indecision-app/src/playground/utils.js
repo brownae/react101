9a3033893b5b659303dec12af8072dee9bfa1068
brownae <aebrown9@gmail.com>
2018-01-03 13:58:36 -0800

    Makes expensify-app the root app on the expensify-app-branch

--- indecision-app/src/playground/utils.js
+++
@@ -1,8 +0,0 @@
-console.log('Utils.js is running!');
-
-const square = (x) => x * x;
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-
-export { square, add, subtract as default }; // this is for passing in various things we want to export.
-// exports - 1. default export 2. named exports
