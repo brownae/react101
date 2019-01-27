6a660993badfe7b5de170ebe1c7999887b5b9c72
brownae <aebrown9@gmail.com>
2019-01-27 08:43:18 -0800

    updates depenencies to work with node-sass and babel core

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000; //The port location provided by Heroku. if not found then use port 3000.
 
 app.use(express.static(publicPath));
 
 //req = request res=response
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(port,() => {
-    console.log('Express server is up.');
+app.listen(port, () => {
+  console.log("Express server is up.");
 });
--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { addExpense,removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
-import './styles/styles.sass';
-import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense, removeExpense, editExpense } from "./actions/expenses";
+import {
+  setTextFilter,
+  sortByDate,
+  sortByAmount,
+  setStartDate,
+  setEndDate
+} from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.sass";
+import "react-dates/lib/css/_datepicker.css";
+import "./firebase/firebase";
 
 const store = configureStore();
 
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById("app"));
